e142b5e723da8f6b0e0d9c22b8734c8d456d8285
orangeonion <thanossprite@gmail.com>
2022-04-30 21:48:59 -0600

    janky ass gravity
    this sum WACK ass gravity, gon fix it but whatever ig

--- player.gd
+++ player.gd
@@ -1,14 +1,19 @@
 extends KinematicBody2D
 
+# movement shit
 export (float) var maxSpd;
 export (float) var grav;
 export (float) var accel;
 export (float) var frict;
 export (float) var airRes; # air resistance
 export (float) var jumpForce; # heheheh kinda like the game
+# gravit var(s)
+#export (PackedScene) var gravPointScene;
 
 var velocity = Vector2.ZERO;
 var spd = 0;
+var magnet = null;
+onready var magnetized = false;
 
 func _ready():
 	pass;
@@ -22,7 +27,9 @@ func _physics_process(delta):
 		velocity.x = spd * delta;
 	
 	# just jumping tingz :zany_face:
-	velocity.y += grav * delta; # psst this is the gravity line
+	velocity.y += grav * delta;
+	if magnet != null:
+		velocity = position.direction_to(magnet.position) * (spd/2);
 	if is_on_floor():
 		if inputDir == 0:
 			spd = lerp(spd, 0, frict);
@@ -34,8 +41,13 @@ func _physics_process(delta):
 			spd = lerp(spd, 0, airRes);
 			velocity.x = spd * delta;
 	
-	
 	velocity = move_and_slide(velocity, Vector2.UP); # DO NOT TOUCH THIS NOBODY WILL EVER TOUCH THIS 
 	#LINE OF CODE IS HOLDING EVERYTHING TOGETHER IF YOU DELETE IT LITTERALLY EVERYTHING WILL BREAK AND 
 	#YOUR FAMILY WILL BURN AND DIE AND YOURBONES WILL DEFLATE AND YOU DOG WILL DIE AND GOD WILL SMITE 
 	# YOU DO NOT DELETE THIS LINE FOR ALL THAT IS HOLY ARK PLS DO NOT DELETE THIS LINE
+
+func _on_Area2D_area_entered(area):
+	magnet = area;
+
+func _on_Area2D_area_exited(area):
+	magnet = null;
