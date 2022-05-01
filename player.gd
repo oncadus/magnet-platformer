260f718433f50e270d9d8dbd89f3359e2674f62d
orangeonion <thanossprite@gmail.com>
2022-05-01 16:49:21 -0600

    "fixed" gravity
    still very buggy, perhaps even WORSE in forms of bugs
    
    but now i have full understanding and control of the code, AND it works from most sides and angles
    
    level design must make note of bugs

--- player.gd
+++ player.gd
@@ -11,42 +11,41 @@ export (float) var jumpForce; # heheheh kinda like the game
 #export (PackedScene) var gravPointScene;
 
 var velocity = Vector2.ZERO;
-var spd = 0;
 var magnet = null;
+var spd = 0;
 
 func _ready():
 	pass;
 
 func _physics_process(delta):
-	# sorta complicated-ish, all you need to know is, if (right button) pressed: move to the right
-	# really it could be shorted a shit ton, but i *reallllyyy* wanted the movement to be supa smooth
 	var inputDir = Input.get_action_strength("right") - Input.get_action_strength("left");
-	if inputDir != 0:
-		spd = lerp(spd, maxSpd * inputDir, accel);
-		velocity.x = spd * delta;
-	
-	# just jumping tingz :zany_face:
 	velocity.y += grav * delta;
 	if magnet != null:
-		velocity = position.direction_to(magnet.position);
-	if is_on_floor():
-		if inputDir == 0:
-			spd = lerp(spd, 0, frict);
-			velocity.x = spd * delta;
+		velocity = position.direction_to(magnet.position) * (spd * inputDir);
 		if Input.is_action_just_pressed("jump"):
-			velocity.y = -jumpForce * delta;
+			magnet = null;
 	else:
-		if inputDir == 0:
-			spd = lerp(spd, 0, airRes);
+		# sorta complicated-ish, all you need to know is, if (right button) pressed: move to the right
+		# really it could be shorted a shit ton, but i *reallllyyy* wanted the movement to be supa smooth
+		if inputDir != 0:
+			spd = lerp(spd, maxSpd * inputDir, accel);
 			velocity.x = spd * delta;
+		if is_on_floor():
+			if inputDir == 0:
+				spd = lerp(spd, 0, frict);
+				velocity.x = spd * delta;
+			if Input.is_action_just_pressed("jump"):
+				velocity.y = -jumpForce * delta;
+		else:
+			if inputDir == 0:
+				spd = lerp(spd, 0, airRes);
+				velocity.x = spd * delta;
 	
 	velocity = move_and_slide(velocity, Vector2.UP); # DO NOT TOUCH THIS NOBODY WILL EVER TOUCH THIS 
 	#LINE OF CODE IS HOLDING EVERYTHING TOGETHER IF YOU DELETE IT LITTERALLY EVERYTHING WILL BREAK AND 
 	#YOUR FAMILY WILL BURN AND DIE AND YOURBONES WILL DEFLATE AND YOU DOG WILL DIE AND GOD WILL SMITE 
 	# YOU DO NOT DELETE THIS LINE FOR ALL THAT IS HOLY ARK PLS DO NOT DELETE THIS LINE
 
-func _on_Area2D_area_entered(area):
-	magnet = area;
-
-func _on_Area2D_area_exited(area):
-	magnet = null;
+func _on_MagnetField_body_entered(body):
+	if body.get_filename() == "res://gravpoint.tscn":
+		magnet = body;
